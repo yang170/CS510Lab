2f515e8e418627a27acc24f527eadf63a253aea5
Yang Jiang <jyang96069@gmail.com>
2022-04-07 15:15:24 -0500

    initial commit

---
+++ api_request_sample.py
@@ -0,0 +1,7 @@
+# wget -O dblp.json --no-check-certificate "https://inex:qatc2011@guacamole.univ-avignon.fr/dblp1/_search?q=\"Digital assistant\"&size=1"
+import requests
+from requests.auth import HTTPBasicAuth
+
+url = "https://guacamole.univ-avignon.fr/dblp1/_search?q=%22Digital%20assistant%22&size=10"
+resp = requests.get(url, auth=HTTPBasicAuth('inex', 'qatc2011'), verify=False)
+print(resp.content)
